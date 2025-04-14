5d9fbe3b3f06060c521b85ce3e4c4a64e7cada53
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2025-04-14 16:27:20 -0300

    update: add o checkboxes no projeto

--- script.js
+++ script.js
@@ -6,8 +6,21 @@ formulario.addEventListener("submit", function(event) {
     let celular = formulario.celular.value;
     let descricao = formulario.descricao.value;
     let fav_lag = formulario.fav_language.value;
+    
+    const checkboxes = document.querySelectorAll('input[name="ling_dominada"]:checked');
+    const valoresSelecionados = Array.from(checkboxes).map(cb => cb.value);
+    
+    // for(let i = 0; i < checkboxes.length; i++){
+        
+    //     checkboxes[i].addEventListener("click", function() {
+    //         if(checkboxes[i].checked){
+    //             console.log(checkboxes[i].value)
+    //         }
+    //     })
+    // }
 
     console.log(nome, celular, descricao, fav_lag)
     // alert("O nome é "+nome+" e o contanto é "+celular);
-    alert(`O nome é ${nome}, o seu contato é ${celular}, o assunto é ${descricao} e a linguagem favorita é ${fav_lag}`)
+    alert(`O nome é ${nome}, o seu contato é ${celular}, o assunto é ${descricao}, a linguagem favorita é ${fav_lag} e as dominantes são: ${valoresSelecionados.join(", ")}`)
+    
 })
