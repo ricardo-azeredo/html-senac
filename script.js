bc4bc94e8c7c04c8e6d1f2976e97bae50b5027df
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2025-04-14 14:41:20 -0300

    update: contato add radio button

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ formulario.addEventListener("submit", function(event) {
     event.preventDefault();
     let nome = formulario.nome.value;
     let celular = formulario.celular.value;
+    let descricao = formulario.descricao.value;
+    let fav_lag = formulario.fav_language.value;
 
-    console.log(nome, celular)
-    alert("O nome é "+nome+" e o contanto é "+celular);
+    console.log(nome, celular, descricao, fav_lag)
+    // alert("O nome é "+nome+" e o contanto é "+celular);
+    alert(`O nome é ${nome}, o seu contato é ${celular}, o assunto é ${descricao} e a linguagem favorita é ${fav_lag}`)
 })
